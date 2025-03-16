d8854f20606d0b02097837d17341909a8c00e6d6
jahedhossain <jahed.w.dev@gmail.com>
2025-03-16 12:04:29 +0600

    initialize React + TypeScript + Vite project with basic configuration and structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import SerialComponent from './SerialComponent';
+
+function App() {
+  return (
+    <>
+      <h1>sf</h1>
+      <SerialComponent />
+    </>
+  );
+}
+
+export default App;
